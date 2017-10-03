81efe930e2fb97bacbf0095a39b551313e1a5106
igr <mixa_fc@ukr.net>
2017-10-03 11:20:45 +0300

    готов класс Point

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,11 @@
 public class Application {
     public static void main(String[] args){
 
-       Point point = new Point(0.5f, 2.5f);
-       Point point2 = new Point(1.3f, 4.2f);
+       Point point = new Point(5.5f, 2.5f);
+       Point point2 = new Point(6.3f, 4.2f);
         float length = point.calcLength(point2);
 
+        System.out.println(length);
 
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -17,7 +17,7 @@ public class Point {
         res1 = (otherPoint.x - x) * (otherPoint.x - x);
         res2 = (otherPoint.y - y) * (otherPoint.y - y);
 
-        res = (float) Math.sqrt(res1 - res2);
+        res = (float) Math.sqrt(res1 + res2);
         return res;
 
     }
